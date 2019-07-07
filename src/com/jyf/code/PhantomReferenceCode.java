64b66077db03ab4b72650956c5914e7c0e16347d
GamePromptDeveloper <354236984@qq.com>
2019-07-07 09:38:27 +0800

    虚引用

---
+++ src/com/jyf/code/PhantomReferenceCode.java
@@ -0,0 +1,23 @@
+package com.jyf.code;
+
+import java.lang.ref.PhantomReference;
+import java.lang.ref.ReferenceQueue;
+
+public class PhantomReferenceCode {
+    public static void main(String[] args) {
+        Object o1 = new Object();
+        ReferenceQueue<Object> objectReferenceQueue = new ReferenceQueue<>();
+        PhantomReference<Object> objectPhantomReference = new PhantomReference<>(o1, objectReferenceQueue);
+
+        System.out.println(o1);//unnull
+        System.out.println(objectReferenceQueue.poll());//null
+        System.out.println(objectPhantomReference.get());//null
+
+        System.out.println("==========================================");
+        o1 = null;
+        System.gc();
+        System.out.println(o1);//null
+        System.out.println(objectReferenceQueue.poll());//unnull
+        System.out.println(objectPhantomReference.get());//null
+    }
+}
