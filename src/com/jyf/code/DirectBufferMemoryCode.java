a5c5e051ee589089c3194fa94262683babf04caf
PerformerSpace <50739851+PerformerSpace@users.noreply.github.com>
2019-07-11 11:27:01 +0800

    lyyy
    lyyy

--- src/com/jyf/code/DirectBufferMemoryCode.java
+++ src/com/jyf/code/DirectBufferMemoryCode.java
@@ -8,6 +8,10 @@ public class DirectBufferMemoryCode {
     /*
     * Direct buffer memory
     * */
+    
+    /**
+    我是lyy,这是我对你代码的修改
+    */
 
     public static void main(String[] args) {
         System.out.println("配置的maxDirectMemory:"+ (VM.maxDirectMemory()/(double)1024/1024)+"MB");
